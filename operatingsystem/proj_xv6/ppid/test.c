087ad75b907b3b0630db4942afa3ccb2657cc276
이건영 <leeanto01.ky@gmail.com>
2017-10-20 09:43:30 -0700

    First Commit

--- operatingsystem/proj_xv6/ppid/test.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-
-int main (int argc, char *argv[]){
-    printf(1, "My pid is %d\n",getpid());
-    printf(1, "My ppid is %d\n",  getppid());
-    exit();
-}
--- operatingsystem/proj_xv6/user_app.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(int argc, char *argv[]){
-    __asm__("int $128");   
-    return 0;
-    }
